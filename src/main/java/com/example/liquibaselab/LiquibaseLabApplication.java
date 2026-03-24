c7daec90042019b9b9b861ea596961a869dbc14e
joi666 <shaparenkoartem@icloud.com>
2026-03-24 17:28:16 +0200

    Initialized cinema DB liquibase project

---
+++ src/main/java/com/example/liquibaselab/LiquibaseLabApplication.java
@@ -0,0 +1,11 @@
+package com.example.liquibaselab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseLabApplication.class, args);
+    }
+}
